36f2f519d8c61bcfd4d7df7e36937f1f4a0aebfb
RA341 <razerishi1505@gmail.com>
2025-10-26 13:43:51 -0400

    update spec

--- spec/gen-stubs.sh
+++ spec/gen-stubs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Load NVM
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
 # Create output directories
-mkdir -p generated/go generated/web
+mkdir -p generated/dart generated/go generated/web
+
+# Generate Dart gRPC stubs
+#protoc \
+#  --proto_path=./protos/ \
+#  -I./protos \
+#  --dart_out=grpc:./generated/dart \
+#  $(find protos/ -name "*.proto")
 
 # Generate Connect-RPC stubs using buf
 buf generate
